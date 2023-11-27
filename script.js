83571e7202902609ef128ab4b265e772cf6d5723
Neil Casas <neilalfonzcasas@gmail.com>
2023-11-27 23:35:25 +0800

    Added function that displays button content, clear button, backspace button

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-    
+// Create a query selector for all buttons
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operators');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+const backspaceButton = document.querySelector('.backspace');
+
+// Display
+const mainDisplay = document.querySelector('.lower');
+
+// Give event listener to all buttons
+numberButtons.forEach(function(button) {
+
+    // Display button content
+    button.addEventListener("click", function() {
+        const buttonValue = button.textContent;
+        mainDisplay.innerHTML = buttonValue;
+    });
+});
