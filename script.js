2025b9aadb6bf8988bca53109143172f6820348f
Neil Casas <neilalfonzcasas@gmail.com>
2023-11-30 10:31:45 +0800

    Added divide by 0 special case

--- script.js
+++ script.js
@@ -114,10 +114,10 @@ function operate(){
 
         // Compute only int values
         case 'x':
-            result = Math.trunc(multiply(num2, num1));
+            result = multiply(num2, num1);
             break;
         case '÷':
-            result = Math.trunc(divide(num2, num1));
+            result = divide(num2, num1);
             break;
         default:
             result = num1;
@@ -144,9 +144,12 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return Math.trunc(num1 * num2);
 }
 
 function divide(num1, num2){
-    return num1 / num2;
+    if(num2 === 0) 
+        return "error";
+    else
+        return Math.trunc(num1 / num2);
 }
